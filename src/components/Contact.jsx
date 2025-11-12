8ea84f72a7d12c8d1227308b0b88f55ffc9b9ea8
darbazrzgar <darborzgar7@gmail.com>
2025-11-12 15:00:32 +0300

    fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://portfolio-backend-production-0032.up.railway.app/api/contact", {
+      const res = await fetch("https://portfolio-backend-production-b665.up.railway.app/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
